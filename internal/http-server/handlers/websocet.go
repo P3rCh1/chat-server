10addef310d74a86385d784147e5b4dccd6f6755
P3rCh1 <okhantsev.vova@mail.ru>
2025-07-24 02:02:45 +0300

    concurency safenty by chans

--- internal/http-server/handlers/websocet.go
+++ internal/http-server/handlers/websocet.go
@@ -15,11 +15,12 @@ import (
 )
 
 const (
-	maxMessageSize  = 4096 // 4KB
-	writeWait       = 10 * time.Second
-	pongWait        = 60 * time.Second
-	pingPeriod      = (pongWait * 9) / 10
-	maxMessageLen   = 1000
+	maxMessageSize = 4096 // TODO move to config, change log msg
+	writeWait      = 10 * time.Second
+	pongWait       = 60 * time.Second
+	pingPeriod     = (pongWait * 9) / 10
+	maxMessageLen  = 1000
+	maxFailedPings = 3
 )
 
 var upgrader = websocket.Upgrader{
@@ -34,25 +35,18 @@ var upgrader = websocket.Upgrader{
 var onlineInRooms sync.Map
 
 type handler struct {
-	client models.Client
-	conn *websocket.Conn
-	tools models.Tools
-	sendChan chan any
+	client    models.Client
+	conn      *websocket.Conn
+	tools     *models.Tools
+	sendChan  chan *models.Message
 	closeChan chan struct{}
 }
 
-func (h *handler) writer(tools *models.Tools) {
-	ticker := time.NewTicker(pingPeriod)
-	for {
-		select {
-		case msg := <-h.sendChan:
-			h.conn.SetWriteDeadline(time.Now().Add(writeWait))
-
-			if err := h.conn.WriteJSON(msg); err != nil {
-				return
-			}
-		}
-	}
+func newHandler() *handler {
+	var h handler
+	h.sendChan = make(chan *models.Message, 100)
+	h.closeChan = make(chan struct{})
+	return &h
 }
 
 func Websocket(tools *models.Tools) http.HandlerFunc {
@@ -63,103 +57,164 @@ func Websocket(tools *models.Tools) http.HandlerFunc {
 			msg.UserNotFound.Drop(w)
 			return
 		}
-		client := &models.Client{
+		h := newHandler()
+		h.tools = tools
+		h.client = models.Client{
 			UserID: userID,
 			RoomID: 0,
 		}
-		client.Username, err = postgres.NewRepository(tools.DB).GetUsername(userID)
+		h.client.Username, err = postgres.NewRepository(tools.DB).GetUsername(userID)
 		if err != nil {
 			msg.UserNotFound.Drop(w)
 			return
 		}
-		conn, err := upgrader.Upgrade(w, r, nil)
+		h.conn, err = upgrader.Upgrade(w, r, nil)
 		if err != nil {
 			tools.Log.Info("upgrade fail", "error", err.Error())
 			return
 		}
 		tools.Log.Info("new connection", "userID", userID)
-		go handleConn(conn, client, tools)
+		go h.writer()
+		go h.reader()
 	}
 }
 
-func handleConn(conn *websocket.Conn, client *models.Client, tools *models.Tools) {
-	defer close(conn, client, tools)
-	conn.SetReadLimit(4096) //TODO add to config file
+func (h *handler) writer() {
+	defer h.conn.Close()
+	ticker := time.NewTicker(pingPeriod)
+	failedPings := 0
+	for {
+		select {
+		case <-h.closeChan:
+			return
+		default:
+		}
+		select {
+		case msg, ok := <-h.sendChan:
+			if !ok {
+				return
+			}
+			h.conn.SetWriteDeadline(time.Now().Add(writeWait))
+			if err := h.conn.WriteJSON(*msg); err != nil {
+				return
+			}
+		case <-ticker.C:
+			h.conn.SetWriteDeadline(time.Now().Add(writeWait))
+			err := h.conn.WriteControl(websocket.PingMessage, nil, time.Now().Add(writeWait))
+			if err != nil {
+				failedPings++
+				if failedPings > maxFailedPings {
+					return
+				}
+			} else {
+				failedPings = 0
+			}
+		case <-h.closeChan:
+			return
+		}
+	}
+}
+
+func (h *handler) reader() {
+	defer func() {
+		close(h.closeChan)
+		close(h.sendChan)
+		h.leave()
+		h.conn.Close()
+		h.tools.Log.Info("connection closed", "userID", h.client.UserID)
+	}()
+	h.conn.SetReadLimit(maxMessageSize)
+	h.conn.SetReadDeadline(time.Now().Add(pongWait))
+	h.conn.SetPongHandler(func(string) error {
+		h.conn.SetReadDeadline(time.Now().Add(pongWait))
+		return nil
+	})
 	for {
 		var r models.WSRequest
-		if err := conn.ReadJSON(&r); err != nil {
+		if err := h.conn.ReadJSON(&r); err != nil {
 			if websocket.IsUnexpectedCloseError(err) {
-				tools.Log.Info("unexpected close",
-					"userID", client.UserID,
+				h.tools.Log.Info("unexpected close",
+					"userID", h.client.UserID,
 					"error", err)
 			}
 			return
 		}
-		if err := route(&r, conn, client, tools); err != nil {
-			tools.Log.Info("request processing failed",
-				"userID", client.UserID,
+		if err := h.route(&r); err != nil {
+			h.tools.Log.Info("request processing failed",
+				"userID", h.client.UserID,
 				"error", err.Error())
-			if err := conn.WriteJSON(map[string]string{"error": err.Error()}); err != nil {
+			if err := h.conn.WriteJSON(map[string]string{"error": err.Error()}); err != nil {
 				return
 			}
 		}
 	}
 }
 
-func close(conn *websocket.Conn, client *models.Client, tools *models.Tools) {
-	handleLeave(conn, client, tools)
-	conn.Close()
-	tools.Log.Info("connection closed", "userID", client.UserID)
+func (h *handler) sendMessage(message *models.Message) error {
+	select {
+	case <-h.closeChan:
+		return nil
+	default:
+	}
+	select {
+	case h.sendChan <- message:
+		return nil
+	case <-time.After(writeWait):
+		h.tools.Log.Warn("message send timeout",
+			"userID", h.client.UserID,
+			"roomID", h.client.RoomID)
+		return errors.New("the message delivery time has expired")
+	case <-h.closeChan:
+		return nil
+	}
 }
 
-func route(r *models.WSRequest, conn *websocket.Conn, client *models.Client, tools *models.Tools) error {
+func (h *handler) route(r *models.WSRequest) error {
 	switch r.Type {
 	case "message":
 		msg := models.Message{
-			Username: client.Username,
+			Username: h.client.Username,
 			Text:     r.Content,
 		}
-		return handleMsg(client, &msg, tools)
+		return h.message(&msg)
 	case "join":
 		newRoomID, err := strconv.Atoi(r.Content)
 		if err != nil {
 			return errors.New("room id must be number")
 		}
-		return handleJoin(conn, client, newRoomID, tools)
+		return h.join(newRoomID)
 	case "leave":
-		return handleLeave(conn, client, tools)
+		return h.leave()
 	default:
 		return errors.New("invalid operation")
 	}
 }
 
-func broadcastToRoom(client *models.Client, msg *models.Message, tools *models.Tools) error {
-	err := postgres.NewRepository(tools.DB).StoreMsg(client, msg)
+func (h *handler) broadcastToRoom(msg *models.Message) error {
+	err := postgres.NewRepository(h.tools.DB).StoreMsg(&h.client, msg)
 	if err != nil {
 		return err
 	}
-	tools.Log.Info("broadcast",
-		"roomID", client.RoomID,
-		"userID", client.UserID,
+	h.tools.Log.Debug("broadcast",
+		"roomID", h.client.RoomID,
+		"userID", h.client.UserID,
 		"text", msg.Text,
 	)
-	val, ok := onlineInRooms.Load(client.RoomID)
+	val, ok := onlineInRooms.Load(h.client.RoomID)
 	if !ok {
 		return nil
 	}
-	online := val.(*sync.Map)
-	online.Range(func(targetInterface, _ any) bool {
-		conn := targetInterface.(*websocket.Conn)
-		if err := conn.WriteJSON(msg); err != nil {
-			close(conn, client, tools)
-		}
+	inRoom := val.(*sync.Map)
+	inRoom.Range(func(targetInterface, _ any) bool {
+		h := targetInterface.(*handler)
+		h.sendMessage(msg)
 		return true
 	})
 	return nil
 }
 
-func handleMsg(client *models.Client, msg *models.Message, tools *models.Tools) error {
-	if client.RoomID == 0 {
+func (h *handler) message(msg *models.Message) error {
+	if h.client.RoomID == 0 {
 		return errors.New("not connected to any room")
 	}
 	if msg.Text == "" {
@@ -168,43 +223,48 @@ func handleMsg(client *models.Client, msg *models.Message, tools *models.Tools) 
 	if len(msg.Text) > 1000 { //TODO move to config
 		return fmt.Errorf("message should be less %d symbols long", 1000)
 	}
-	return broadcastToRoom(client, msg, tools)
+	h.broadcastToRoom(msg)
+	return nil
 }
 
-func handleJoin(conn *websocket.Conn, client *models.Client, newRoomID int, tools *models.Tools) error {
-	if client.RoomID == newRoomID {
+func (h *handler) join(newRoomID int) error {
+	if h.client.RoomID == newRoomID {
 		return errors.New("already in room")
 	}
-	handleLeave(conn, client, tools)
-	if err := postgres.NewRepository(tools.DB).IsRoomMember(client.UserID, newRoomID); err != nil {
+	h.leave()
+	if err := postgres.NewRepository(h.tools.DB).IsRoomMember(h.client.UserID, newRoomID); err != nil {
 		return err
 	}
-	client.RoomID = newRoomID
+	h.client.RoomID = newRoomID
 	newOnline, ok := onlineInRooms.Load(newRoomID)
 	if !ok {
 		m := new(sync.Map)
-		m.Store(conn, struct{}{})
+		m.Store(h, struct{}{})
 		onlineInRooms.Store(newRoomID, m)
 	} else {
-		newOnline.(*sync.Map).Store(conn, struct{}{})
+		newOnline.(*sync.Map).Store(h, struct{}{})
 	}
 	msg := &models.Message{
-		Username: client.Username,
+		Username: h.client.Username,
 		Text:     "joined the room",
 	}
-	return broadcastToRoom(client, msg, tools)
+	h.broadcastToRoom(msg)
+	return nil
 }
 
-func handleLeave(conn *websocket.Conn, client *models.Client, tools *models.Tools) error {
-	online, ok := onlineInRooms.Load(client.RoomID)
+func (h *handler) leave() error {
+	if h.client.RoomID == 0 {
+		return errors.New("not connected to any room")
+	}
+	online, ok := onlineInRooms.Load(h.client.RoomID)
 	if ok {
-		online.(*sync.Map).Delete(conn)
+		online.(*sync.Map).Delete(h)
 	}
 	msg := &models.Message{
-		Username: client.Username,
+		Username: h.client.Username,
 		Text:     "leaved the room",
 	}
-	err := broadcastToRoom(client, msg, tools)
-	client.RoomID = 0
-	return err
+	h.broadcastToRoom(msg)
+	h.client.RoomID = 0
+	return nil
 }
